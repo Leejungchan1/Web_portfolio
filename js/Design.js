fbf2670205c289636948bb2254dad86a5bdca5dc
Leejungchan1 <wjdcks0123@naver.com>
2024-10-04 17:39:53 +0900

    미완성

---
+++ js/Design.js
@@ -0,0 +1,34 @@
+$(function(){
+    $(".main_inner").slick({
+        rows:1,
+        dots: true,
+        arrows: true,
+
+    });
+
+    $(".r_slide").slick({
+        arrows: true,
+        draggable:false,
+        fade: true
+    });
+
+    $(".popup").hide();
+
+    $(".bottom").click(function() {
+        var index = $(this).attr('class').match(/\d+/)[0];
+        $(".popup-" + index).fadeIn();
+    });
+
+    $(".button").click(function(){
+        $(".popup").fadeOut();
+    })
+
+
+    $(document).mouseup(function (e) { 
+        var container = $(".popup");
+        if (!container.is(e.target) && container.has(e.target).length === 0){
+            container.css("display","none");
+        }	
+    });
+
+})
